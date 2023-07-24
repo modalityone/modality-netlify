02d4def8eb7aa38d0163b77208e715cb15e61a9c
bjvickers <dev.salmonb@gmail.com>
2023-07-24 13:59:27 +0000

    Update from https://github.com/modalityone/modality/commit/745763f6b2ada5288a6829bde3dd62dc708e6d36

--- one/modality/base/shared/domainmodel/formatters/DateFormatter.java
+++ one/modality/base/shared/domainmodel/formatters/DateFormatter.java
@@ -5,50 +5,52 @@ import dev.webfx.extras.type.Type;
 import dev.webfx.platform.util.Dates;
 import dev.webfx.stack.orm.domainmodel.formatter.ValueFormatter;
 import dev.webfx.stack.orm.domainmodel.formatter.ValueParser;
-import java.time.LocalDate;
+
 import javafx.util.StringConverter;
+
+import java.time.LocalDate;
 
 /**
  * @author Bruno Salmon
  */
 public final class DateFormatter implements ValueFormatter, ValueParser {
 
-  public static final DateFormatter SINGLETON = new DateFormatter();
+    public static final DateFormatter SINGLETON = new DateFormatter();
 
-  private DateFormatter() {}
+    private DateFormatter() {}
 
-  @Override
-  public Type getFormattedValueType() {
-    return PrimType.STRING;
-  }
+    @Override
+    public Type getFormattedValueType() {
+        return PrimType.STRING;
+    }
 
-  @Override
-  public Object formatValue(Object value) {
-    return Dates.format(value, "dd/MM/yyyy");
-  }
+    @Override
+    public Object formatValue(Object value) {
+        return Dates.format(value, "dd/MM/yyyy");
+    }
 
-  @Override
-  public Object parseValue(Object value) {
-    if (value == null) return null;
-    String text = value.toString();
-    int p;
-    int dayOfMonth = Integer.parseInt(text.substring(0, p = text.indexOf('/')));
-    int month = Integer.parseInt(text.substring(p + 1, p = text.indexOf('/', p + 1)));
-    int year = Integer.parseInt(text.substring(p + 1, p + 5));
-    return LocalDate.of(year, month, dayOfMonth);
-  }
+    @Override
+    public Object parseValue(Object value) {
+        if (value == null) return null;
+        String text = value.toString();
+        int p;
+        int dayOfMonth = Integer.parseInt(text.substring(0, p = text.indexOf('/')));
+        int month = Integer.parseInt(text.substring(p + 1, p = text.indexOf('/', p + 1)));
+        int year = Integer.parseInt(text.substring(p + 1, p + 5));
+        return LocalDate.of(year, month, dayOfMonth);
+    }
 
-  public StringConverter<LocalDate> toStringConverter() {
-    return new StringConverter<LocalDate /*GWT*/>() {
-      @Override
-      public String toString(LocalDate date) {
-        return (String) formatValue(date);
-      }
+    public StringConverter<LocalDate> toStringConverter() {
+        return new StringConverter<LocalDate /*GWT*/>() {
+            @Override
+            public String toString(LocalDate date) {
+                return (String) formatValue(date);
+            }
 
-      @Override
-      public LocalDate fromString(String date) {
-        return (LocalDate) parseValue(date);
-      }
-    };
-  }
+            @Override
+            public LocalDate fromString(String date) {
+                return (LocalDate) parseValue(date);
+            }
+        };
+    }
 }
